212bf8e3c457a1faa4a533c73d81d73fc5873b83
Christian Chown <christianchown@gmail.com>
2018-08-14 17:48:21 +0100

    Remove ts warning - https://github.com/redux-loop/redux-loop/issues/147

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,8 +1,8 @@
 import {createStore} from 'redux';
-import {install} from 'redux-loop';
+import {install, StoreCreator} from 'redux-loop';
 import reducer, {StoreState, initialState} from './reducer';
 import {Auth} from './auth';
 import {Route, Router} from './router';
 
 export {Auth, Route, Router, StoreState};
-export default createStore(reducer, initialState, install());
+export default (createStore as StoreCreator)(reducer, initialState, install());
